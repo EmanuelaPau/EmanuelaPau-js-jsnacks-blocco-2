7c9be41e41d7fda6fab2215b708a5c70e334c2fe
EmanuelaPau <uapame@gmail.com>
2023-05-19 17:37:30 +0200

    Snack 2

---
+++ snack 2/script/my_script.js
@@ -0,0 +1,22 @@
+// Generare numero casuale tra 1 e 100 COMPRESI. Continuare a chiedere all'utente di indovinare il numero fino a quando non lo indovina.
+// Ogni volta che sbaglia stampiamo in console un messaggio che gli/le dice se il numero da indovinare è maggiore o minore.
+// Una volta che ha indovinato, stampiamo in console il numero di tentativi totali.
+
+let MyRandomNumber = Math.floor(Math.random() * 100);
+console.log(MyRandomNumber);
+
+let yourGuess;
+let yourTry = 0
+
+while (MyRandomNumber !== yourGuess) {
+    yourGuess = parseInt(prompt('guess the number'));
+    if (yourGuess < MyRandomNumber) {
+        console.log("il numero che hai messo è minore di quello che devi indovinare");
+    } else if (yourGuess > MyRandomNumber) {
+        console.log("il numero che hai messo è maggiore di quello che devi indovinare");
+    }
+
+    yourTry = yourTry + 1;
+}
+
+console.log(`bravo, hai indovinato in ${yourTry} tentativi`);
