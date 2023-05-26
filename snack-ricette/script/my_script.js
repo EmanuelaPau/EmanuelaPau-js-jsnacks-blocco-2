65a144a9935b7e0f1a0cec472b5791dd445418af
EmanuelaPau <uapame@gmail.com>
2023-05-26 12:35:11 +0200

    Add first button funzionality

--- snack-ricette/script/my_script.js
+++ snack-ricette/script/my_script.js
@@ -1,2 +1,24 @@
 console.log('hello snack');
 
+// ? Dati un input e due bottoni, l'utente popola una lista di ingredienti usando il primo bottone, al termine della quale preme un secondo bottone e viene generata un'immagine casuale di un pasto al centro dello schermo dopo 2 secondi per ogni ingrediente nella ricetta..
+
+const myInput = document.querySelector('.form-input');
+const mySubimitListButton = document.querySelector('#button-add-recipe');
+const myGenerateRecipeButton = document.querySelector('#button-stylize-recipe');
+
+const myUlElement = document.querySelector('.list');
+const myIngredientsList = [];
+
+mySubimitListButton.addEventListener('click', function () {
+    const myInputValue = myInput.value;
+    myIngredientsList.push(myInputValue);
+    console.log(myIngredientsList);
+
+    for (let i = 0; i < myIngredientsList.length; i++) {
+        myUlElement.innerHTML = `
+        <li>
+            ${myIngredientsList[i]} 
+        </li>
+        `;
+    }
+}); 
