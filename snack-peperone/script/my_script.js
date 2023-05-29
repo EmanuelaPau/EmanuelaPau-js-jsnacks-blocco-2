80f4631e799dabaf9a5b0b59fa4a7374c3ea1965
EmanuelaPau <uapame@gmail.com>
2023-05-29 11:58:28 +0200

    snack peperoni

---
+++ snack-peperone/script/my_script.js
@@ -0,0 +1,91 @@
+console.log('hello snack');
+
+// let hisShots;
+
+// console.log(hisShots);
+
+// let allLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// function getRandomString(randomLetter, randomNumbers) {
+
+// }
+
+// const basketPlayer = {
+//     playerId: 2,
+//     name: "LeBron",
+//     surname: "James",
+//     age: 38,
+//     scoreAverage: Math.floor(Math.random() * 30) + 1,
+//     "average 3 points free shooting": Math.floor(Math.random() * 100) + 1,
+//     stops: Math.floor(Math.random() * 30 + 1) + 1,
+//     shots: Math.floor(Math.random() * (100 - 20 + 1)) + 20
+// }
+
+// console.log(basketPlayer);
+
+// Crea un array di 10 oggetti che rappresentano un peperone, indicando per ognuno varietà, peso e lunghezza. Calcola quanto pesano tutti i peperoni.
+
+// Bonus:
+// crea un peperone con una funzione
+
+let myPeperoni = [
+    {
+        variety: "red",
+        weight: "100gr",
+        lenght: "10cm"
+    },
+
+    {
+        variety: "red",
+        weight: "100gr",
+        lenght: "10cm"
+    },
+
+    {
+        variety: "red",
+        weight: "100gr",
+        lenght: "10cm"
+    },
+
+    {
+        variety: "red",
+        weight: "100gr",
+        lenght: "10cm"
+    },
+
+    {
+        variety: "red",
+        weight: "100gr",
+        lenght: "10cm"
+    }
+
+    {
+        variety: "red",
+        weight: "100gr",
+        lenght: "10cm"
+    },
+
+    {
+        variety: "red",
+        weight: "100gr",
+        lenght: "10cm"
+    },
+
+    {
+        variety: "red",
+        weight: "100gr",
+        lenght: "10cm"
+    },
+
+    {
+        variety: "red",
+        weight: "100gr",
+        lenght: "10cm"
+    },
+
+    {
+        variety: "red",
+        weight: "100gr",
+        lenght: "10cm"
+    }
+]
